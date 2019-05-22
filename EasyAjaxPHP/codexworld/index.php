61c45ea75aee3f8a7a3bd92cfc6374f5cfd9c8e5
wujianhua2000 <wujianhua2000@hotmail.com>
2019-05-22 18:03:40 +0800

    4nd demo

---
+++ EasyAjaxPHP/codexworld/index.php
@@ -0,0 +1,11 @@
+
+<input type="text" 		id="user_id" 							/>
+<input type="button" 	id="getUser" 	value="Get Details"		/>
+
+<div class="user-content" style="display: none;"	>
+    <h4>User Details</h4>
+    <p>Name: 			<span id="userName"		></span></p>
+    <p>Email: 			<span id="userEmail"	></span></p>
+    <p>Phone: 			<span id="userPhone"	></span></p>
+    <p>Register Date: 	<span id="userCreated"	></span></p>
+</div>
---
+++ EasyAjaxPHP/codexworld/user-click.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function(){
+
+    $('#getUser').on('click',function(){
+	
+        var user_id = $('#user_id').val();
+
+        $.ajax({
+            type		:	'POST',
+            url			:	'getData.php',
+            dataType	: 	"json",
+            data		:	{user_id:user_id},
+        
+			success		:	function(data){
+			
+                if ( data.status == 'ok' ) {
+                    $( '#userName'		).text( data.result.name	);
+                    $( '#userEmail'		).text( data.result.email	);
+                    $( '#userPhone'		).text( data.result.phone	);
+                    $( '#userCreated'	).text( data.result.created	);
+                    $( '.user-content'	).slideDown();
+                }else{
+                    $( '.user-content'	).slideUp();
+                    alert( "User not found..." );
+                } 
+            }
+			
+        });
+		
+    });
+});
